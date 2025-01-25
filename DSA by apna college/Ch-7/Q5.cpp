7702beee72dc7d6ef7f0c79b7662520869d900f0
Mohitraj72 <digitalmcsc12345@gmail.com>
2025-01-25 10:25:21 +0530

    'update'

--- DSA by apna college/Ch-7/Q5.cpp
+++ DSA by apna college/Ch-7/Q5.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int factorial (int n){
-    int fact =1;
-    for (int i=1; i<=n; i++){
-        fact = fact *i;
+int factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
     }
 
-    cout<<"Factorial ("<<n<<")= " << fact << endl;
+    cout << "Factorial (" << n << ")= " << fact << endl;
     return fact;
 }
 
-
-int main(){
+int main()
+{
 
     factorial(3);
-    
-    
-    
+
     return 0;
 }
---
+++ DSA by apna college/Ch-7/Q6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// Memory Stack 
+
+int main(){
+
+    
+    
+    
+    
+    return 0;
+}
