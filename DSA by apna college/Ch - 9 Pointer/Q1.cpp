877a71307fe70e0af063e2e71b916b0b0e6284a9
Mohitraj72 <digitalmcsc12345@gmail.com>
2025-03-30 20:08:34 +0530

    'update'

--- DSA by apna college/Ch - 9 Pointer/Q1.cpp
+++ DSA by apna college/Ch - 9 Pointer/Q1.cpp
@@ -7,15 +7,19 @@ int main()
     int a = 10;
     int *ptr = &a;
 
-    float pi = 3.14;
-    float *ptr2 = &pi;
+    int **pptr = &ptr;
+
+    cout << &ptr << " = " << pptr << "\n";
+
+    // float pi = 3.14;
+    // float *ptr2 = &pi;
 
     // cout << &pi << " = " << ptr2 << "\n";
     // cout << &a << " = " << ptr << "\n";
 
-    cout << sizeof(ptr) << "\n";
-    cout << sizeof(ptr2) << "\n";
+    // cout << sizeof(ptr) << "\n";
+    // cout << sizeof(ptr2) << "\n";
 
-    
+
     return 0;
 }
