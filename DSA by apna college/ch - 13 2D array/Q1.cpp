5ada1e0a3458994e4022aeb825e53605f86cc7c5
Mohitraj72 <digitalmcsc12345@gmail.com>
2025-05-22 09:38:30 +0530

    'update'

---
+++ DSA by apna college/ch - 13 2D array/Q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int students[3][3] = {{100, 100, 100},
+                          {85, 74, 89},
+                          {68, 78, 44}};
+
+    cout << students[1][1] << endl;
+
+    return 0;
+}
