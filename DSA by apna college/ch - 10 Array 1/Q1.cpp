0c1a908ea268d060153231df88162acd259b1581
Mohitraj72 <digitalmcsc12345@gmail.com>
2025-05-22 11:02:13 +0530

    'update'

--- DSA by apna college/ch - 10 Array 1/Q1.cpp
+++ DSA by apna college/ch - 10 Array 1/Q1.cpp
@@ -6,16 +6,5 @@ int main(){
     int marks[50] = {10,20,22,14};
     
     
-    455
-    
-    wewrtty
-    4555
-    erttyyt
-    oppp
-    lll
-    pp
-    rtrt
-    45tyu
-    5678
     return 0;
 }
